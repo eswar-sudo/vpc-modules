e535b8e263706ed9c75b21de1a9cf111af6d9d17
eswar21-sudo <eswarkumar5475@gmail.com>
2025-03-21 05:25:02 +0000

    modified

--- vpc-modules/modules/vpc/main.tf
+++ vpc-modules/modules/vpc/main.tf
@@ -4,13 +4,13 @@ resource "aws_vpc" "main" {
   enable_dns_support = true
   enable_dns_hostnames = true
   tags = {
-     Name = "var.vpc_name" 
+     Name = var.vpc_name 
   }
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
   tags = { 
-    Name = "var.vpc_name-igw" 
+    Name = var.vpc_name-igw
   }
 }
