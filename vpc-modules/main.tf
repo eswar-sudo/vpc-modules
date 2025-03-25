92d85b17ae7c07bb3c6689e8d82a5a4fee9c6527
eswar21-sudo <eswarkumar5475@gmail.com>
2025-03-25 11:39:13 +0530

    Update main.tf

--- vpc-modules/main.tf
+++ vpc-modules/main.tf
@@ -1,9 +1,9 @@
 module "vpc" {
   source    = "./modules/vpc"
   vpc_cidr  = var.vpc_cidr
-  region = var.region
-  vpc_name = var.vpc_name
-  enable_dns_support = var.enable_dns_support
+  region    = var.region
+  vpc_name  = var.vpc_name
+  enable_dns_support   = var.enable_dns_support
   enable_dns_hostnames = var.enable_dns_hostnames
 }
   
@@ -11,28 +11,28 @@ module "subnets" {
   source   = "./modules/subnets"
   vpc_id   = module.vpc.vpc_id
   azs      = var.azs
-  vpc_cidr = var.vpc_cidr
-  map_public_ip_on_launch = var.map_public_ip_on_launch
+  vpc_cidr = var.vpc_cidr  
   vpc_name = var.vpc_name
+  map_public_ip_on_launch = var.map_public_ip_on_launch
 }
 
 module "nat" {
   source              = "./modules/nat"
   public_subnet_ids   = module.subnets.public_subnet_ids
   single_nat_gateway  = var.single_nat_gateway
-  vpc_name = var.vpc_name
+  vpc_name            = var.vpc_name
 }
 
 module "routes" {
-  source            = "./modules/routes"
+  source           = "./modules/routes"
   vpc_id           = module.vpc.vpc_id
-  vpc_name = var.vpc_name
-  public_subnet_ids = module.subnets.public_subnet_ids
-  private_subnet_ids = module.subnets.private_subnet_ids
-  public_destination_cidr_block = var.public_destination_cidr_block
-  private_destination_cidr_block = var.private_destination_cidr_block
+  vpc_name         = var.vpc_name
+  azs              = var.azs
   igw_id           = module.vpc.igw_id
   nat_gateway_ids  = module.nat.nat_gateway_ids
   single_nat_gateway  = var.single_nat_gateway
-  azs  = var.azs
+  public_subnet_ids   = module.subnets.public_subnet_ids
+  private_subnet_ids  = module.subnets.private_subnet_ids
+  public_destination_cidr_block  = var.public_destination_cidr_block
+  private_destination_cidr_block = var.private_destination_cidr_block
 }
