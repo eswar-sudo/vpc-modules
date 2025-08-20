bc2c17ae20f7a26d878017b74152ef9ff4cda1c1
eswar-sudo <eswarkumar501@gmail.com>
2025-08-20 21:39:16 +0530

    Update outputs.tf

--- vpc-modules/outputs.tf
+++ vpc-modules/outputs.tf
@@ -1,29 +1,39 @@
 output "vpc_id" {
   description = "The ID of the VPC"
-  value       = module.vpc-modules.vpc_id
-}
-
-output "private_subnet_ids" {
-  description = "List of private subnet IDs"
-  value       = module.vpc-modules.private_subnet_ids
-}
-
-output "public_subnet_ids" {
-  description = "List of public subnet IDs"
-  value       = module.vpc-modules.public_subnet_ids
-}
-
-output "nat_gateway_ids" {
-  description = "IDs of the NAT Gateways"
-  value       = module.vpc-modules.nat_gateway_ids
+  value       = module.vpc.vpc_id
 }
 
 output "igw_id" {
-  description = "ID of the Internet Gateway"
-  value       = module.vpc-modules.igw_id
+  description = "The ID of the Internet Gateway" 
+  value       = module.vpc.igw_id
 }
 
-output "route_table_ids" {
-  description = "IDs of the route tables"
-  value       = module.vpc-modules.route_table_ids
+output "public_subnet_ids" {
+  description = "List of public subnet IDs"      
+  value       = module.subnets.public_subnet_ids 
 }
+
+output "private_subnet_ids" {
+  description = "List of private subnet IDs"     
+  value       = module.subnets.private_subnet_ids
+}
+
+output "nat_gateway_ids" {
+  description = "List of NAT Gateway IDs"        
+  value       = module.nat.nat_gateway_ids      
+}
+
+output "eip_ids" {
+  description = "List of EIP IDs"
+    value = module.nat.eip_ids
+  }
+
+output "public_route_table_ids" {
+  description = "List of public RT IDs"
+  value = module.routes.public_route_table_ids
+}
+
+   output "private_route_table_ids" {
+     description = "List of private RT IDs"
+     value = module.routes.private_route_table_ids
+ }
